844ebe8a1ccbf910dfcb35880c128cc2fe00885c
Radiet Billahirabba <radiet@41studio.com>
2016-11-10 14:17:27 +0700

    Add ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps : [
+    {
+      name      : "auction_app",
+      script    : "/usr/local/bin/npm",
+      cwd       : "/home/vagrant/apps/AuctionApp",
+      args      : "run start:prod"
+    },
+  ],
+
+  deploy : {
+    production : {
+      user : "vagrant",
+      host : "192.168.33.10",
+      ref  : "origin/master",
+      repo : "git@github.com:Radiet/AuctionApp.git",
+      path : "/home/vagrant/apps"
+    },
+  }
+}
